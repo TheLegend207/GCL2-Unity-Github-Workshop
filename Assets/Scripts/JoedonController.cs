e5a57be62518a07a036d06fcbeba29b1fedd16f0
JohnOriginalDude <cooljoedon@gmail.com>
2026-04-29 11:27:08 +0800

    Joedon, Added sprites and moved GameObjects

---
+++ Assets/Scripts/JoedonController.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class JoedonController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Health restored!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
