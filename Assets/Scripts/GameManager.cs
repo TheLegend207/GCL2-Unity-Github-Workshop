ff21c2f2276639168565a7946d0beeadbe239fd1
VeejnasYT <122197454+VeejnasYT@users.noreply.github.com>
2026-05-06 09:57:56 +0800

    Sanjeev, Changed lives back to 4

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
 
     [Header("Game Settings")]
     public int startingScore = 100;   // <-- Activity 2 script conflict target
-    public int lives = 5;           
+    public int lives = 4;           
     public float gameSpeed = 1.0f;
 
     private void Awake()
