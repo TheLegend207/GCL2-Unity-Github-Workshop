064e82087341fa0a30e4a82e54e02ba7c1b41725
JoshyMoshy07 <joshuasong1925@gmail.com>
2026-05-06 10:41:19 +0800

    Joshua, renaming previous object to game manager and adding lives to script

--- Assets/Scripts/JoshuaController.cs
+++ Assets/Scripts/JoshuaController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class JoshuaController : MonoBehaviour
 {
+    public int lives;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         print ("Mystery Box activated!");
+
     }
 
     // Update is called once per frame
